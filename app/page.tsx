c57126bc0877110d176c0234fdc7da2a6aada51d
tamirlan_laptop <laptop@local>
2025-05-20 21:31:14 +0100

    feat: added a link to the chat with AI

--- app/page.tsx
+++ app/page.tsx
@@ -66,8 +66,10 @@ export default function Home() {
 
       <div className="mt-8">
         <Snippet hideCopyButton hideSymbol variant="bordered">
-          <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
+          <span style={{ fontSize: "1.5rem" }}>
+            Get started by  <Link href="/aichat" style={{ fontSize: "1.5rem", color: "#06b6d4", textDecoration: "underline" }}>
+              talking to our AI.
+            </Link>
           </span>
         </Snippet>
       </div>
