ff6fafd03f57d09e56359332976f3f5d9571e75d
tamirlan_laptop <laptop@local>
2025-05-20 21:10:12 +0100

    feat: removed the search bar from the main page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -27,27 +27,11 @@ import {
   UserIcon,
 } from "@/components/icons";
 
+// ...existing imports...
+
 export const Navbar = () => {
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
+  // Remove the searchInput definition
+  // const searchInput = ( ... );
 
   return (
     <HeroUINavbar maxWidth="xl" position="sticky">
@@ -86,7 +70,7 @@ export const Navbar = () => {
           </Link>
           <ThemeSwitch />
         </NavbarItem>
-        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
+        {/* Removed: <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem> */}
         <NavbarItem className="hidden md:flex">
           <Button
             isExternal
@@ -111,7 +95,7 @@ export const Navbar = () => {
       </NavbarContent>
 
       <NavbarMenu>
-        {searchInput}
+        {/* Removed: {searchInput} */}
         <div className="mx-4 mt-2 flex flex-col gap-2">
           {siteConfig.navMenuItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
