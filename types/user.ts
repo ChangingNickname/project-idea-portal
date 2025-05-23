ec79c3258c87d35c5d3ad398ef0181e18adc215b
tamirlan_pc <pc@local>
2025-05-23 09:23:01 +0500

    Create types for user

---
+++ types/user.ts
@@ -0,0 +1,39 @@
+interface UserProviderData {
+  providerId: string;
+  uid: string;
+  displayName: string | null;
+  email: string | null;
+  phoneNumber: string | null;
+  photoURL: string | null;
+}
+
+interface UserMetadata {
+  creationTime: string;
+  lastSignInTime: string;
+  lastRefreshTime: string;
+}
+
+interface MultiFactorInfo {
+  uid: string;
+  factorId: string;
+  displayName: string | null;
+  enrollmentTime: string;
+}
+
+export interface User {
+  uid: string;
+  email: string | null;
+  emailVerified: boolean;
+  displayName: string | null;
+  photoURL: string | null;
+  phoneNumber: string | null;
+  disabled: boolean;
+  isAnonymous: boolean;
+  providerData: UserProviderData[];
+  customClaims: Record<string, any> | null;
+  metadata: UserMetadata;
+  tenantId: string | null;
+  multiFactor?: {
+    enrolledFactors: MultiFactorInfo[];
+  } | null;
+}
