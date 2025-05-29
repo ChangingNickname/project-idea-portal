fe354e130f39860cac0b1f796883d757bf4c08ba
tamirlan_pc <pc@local>
2025-05-29 13:57:26 +0500

    Add LAyouts
    Add pinia
    
    Add i18n

---
+++ app/layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1> test </h1>
+        <div>
+            <slot />
+        </div>
+    </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,9 @@ export default defineNuxtConfig({
 
   modules: [
     '@nuxt/ui',
-    '@nuxt/eslint'
+    '@nuxt/eslint',
+    '@pinia/nuxt',
+    '@nuxtjs/i18n'
   ],
 
   css: ['~/assets/css/main.css'],
