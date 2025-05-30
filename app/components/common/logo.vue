7ddba8b8ded65c0d28bb40cc51cd1d2635889f35
tamirlan_pc <pc@local>
2025-05-30 09:37:43 +0500

    fix: update logo component to use Icon from #components

--- app/components/common/logo.vue
+++ app/components/common/logo.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="flex items-center gap-2">
     <span class="flex items-center justify-center w-10 h-10 border-2 border-neutral-100 dark:border-neutral-900 rounded-full bg-neutral-900 dark:bg-neutral-100">
-      <UIcon name="i-lucide-lightbulb" class="text-neutral-100 dark:text-neutral-900 w-[22px] h-[22px]" />
+      <Icon name="lucide:lightbulb" class="text-neutral-100 dark:text-neutral-900 w-[22px] h-[22px]" />
     </span>
     <span class="dark:text-neutral-100 text-neutral-900 font-bold text-2xl font-sans tracking-wide">PIP</span>
   </div>
 </template>
+
+<script setup lang="ts">
+import { Icon } from '#components'
+</script>
