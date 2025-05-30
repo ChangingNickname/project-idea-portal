f479c39637aae8a9fcb0a242dae422e4cda4d063
tamirlan_pc <pc@local>
2025-05-30 09:39:02 +0500

    feat: add auth pages for login and registration

---
+++ app/pages/auth/login.vue
@@ -0,0 +1,9 @@
+<template>
+    <UserLogin
+        v-model="isOpen"
+    />
+</template>
+
+<script setup lang="ts">
+const isOpen = ref(true)
+</script> 
---
+++ app/pages/auth/register.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="min-h-screen bg-gray-50 dark:bg-gray-900">
+    test
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
