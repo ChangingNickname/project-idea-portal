5a459a52a371d7242113d4e096af3d253f66ad58
tamirlan_pc <pc@local>
2025-06-20 12:21:04 +0500

    Highlight PIP

--- app/components/common/logo.vue
+++ app/components/common/logo.vue
@@ -3,7 +3,10 @@
     <span class="flex items-center justify-center w-10 h-10 border-2 border-neutral-100 dark:border-neutral-900 rounded-full bg-neutral-900 dark:bg-neutral-100">
       <Icon name="lucide:lightbulb" class="text-neutral-100 dark:text-neutral-900 w-[22px] h-[22px]" />
     </span>
-    <span class="dark:text-neutral-100 text-neutral-900 font-bold text-2xl font-sans tracking-wide">Project Ideas Portal</span>
+    <span class="dark:text-neutral-100 text-neutral-900 font-bold text-2xl font-sans tracking-wide">
+      <span class="text-primary-500">P</span>roject 
+      <span class="text-primary-500">I</span>deas 
+      <span class="text-primary-500">P</span>ortal</span>
   </div>
 </template>
 
